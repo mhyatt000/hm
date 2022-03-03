fc0b94cf2df03f80dcda8cf64e4cad0605b005c0
Matthew Hyatt <mhyatt000@gmail.com>
2022-03-02 18:47:39 -0600

    temp

--- main.py
+++ main.py
@@ -2,9 +2,32 @@ import tensorflow as tf
 import sklearn
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from visualize import scatter
 
 
-def main(): pass
+def main():
+
+    path_articles = 'articles.csv'
+
+    articles = pd.read_csv(path_articles)
+
+    # remove words for now?
+    articles = articles.drop(labels='detail_desc', axis=1)
+
+    print(articles)
+    print(articles.columns)
+
+    data = {'product_type_no': articles['product_type_no'].tolist(),
+            'garment_group_no': articles['garment_group_no'].tolist()}
+
+    print(data_dict)
+
+    fig, ax = plt.subplots()
+    ax.scatter(articles['product_type_no'].tolist(), articles['garment_group_no'].tolist())
+    plt.show()
+
+    scatter(data)
 
 
 if __name__ == '__main__':
--- visualize.py
+++ visualize.py
@@ -1,6 +1,14 @@
 import matplotlib.pyplot as plt
 
 
+def scatter(*, data, labels):
+
+    fig, ax = plt.subplots()
+    ax.scatter(data[0], data[1])
+    ax.set(xlabel=labels[0], ylabel=labels[1])
+    plt.show()
+
+
 def main(): pass
 
 
