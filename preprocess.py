b666e87ac4558b58f0c8edcabe9268004b292700
Matthew Hyatt <mhyatt000@gmail.com>
2022-03-23 20:12:56 -0500

    temp

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,6 @@ def article_mapper():
     '''maps articles ids to their corresponding index'''
 
     data = pd.read_csv('articles.csv')
-
     map = [i for i in data.index]
 
     articles = data['article_id'].to_list()
@@ -15,8 +14,20 @@ def article_mapper():
     return map
 
 
+def customer_mapper():
+    '''like the article mapper but for customers'''
+
+    data = pd.read_csv('customers.csv')
+    map = [i for i in data.index]
+
+    articles = data['customer_id'].to_list()
+
+    map = {a: m for a, m in zip(articles, map)}
+    return map
+
+
 def main():
-    article_mapper()
+    customer_mapper()
 
 
 if __name__ == '__main__':
